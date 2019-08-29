2a612a2cff75a54e919f42aab36fef2d87400bc7
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-08-29 15:17:55 +0530

    Add files via upload
    Python program to find total number of maxima and minima in the given number. This can also be asked to find the total number of slopes of a given number.

---
+++ SlopeOfGivenNumber.py
@@ -0,0 +1,9 @@
+var = input()
+values = [int(s) for s in var]
+counter = 0
+for i in range(0,len(values)-2):
+    if((values[i]<values[i+1])and(values[i+1]<values[i+2])):
+        counter += 1
+    elif((values[i]>values[i+1])and(values[i+1]>values[i+2])):
+        counter += 1
+print(counter*2)
